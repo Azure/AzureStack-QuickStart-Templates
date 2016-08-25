5dff3d69f819ff0a447e2151a5749db08b883d22
srimathiS <srimathi.santhanam@microsoft.com>
2016-08-25 10:50:24 -0700

    parameter cleanup for simple Linux, mesos, docker

--- docker-swarm-ubuntu/deploySwarm.ps1
+++ docker-swarm-ubuntu/deploySwarm.ps1
@@ -2,16 +2,16 @@
 $endptOut = Invoke-RestMethod "$("https://api.$env:USERDNSDOMAIN".ToLowerInvariant())/metadata/endpoints?api-version=1.0"
 $envName = "AzureStackCloud"
 Add-AzureRmEnvironment -Name ($envName) `
-	                -ActiveDirectoryEndpoint ($ActiveDirectoryEndpoint = $($endptOut.authentication.loginEndpoint) + $($endptOut.authentication.audiences[0]).Split("/")[-1] + "/") `
-	                -ActiveDirectoryServiceEndpointResourceId ($ActiveDirectoryServiceEndpointResourceId = $($endptOut.authentication.audiences[0])) `
-	                -ResourceManagerEndpoint ($ResourceManagerEndpoint = $("https://api.$env:USERDNSDOMAIN".ToLowerInvariant())) `
-	                -GalleryEndpoint ($GalleryEndpoint = $endptOut.galleryEndpoint) `
-	                -GraphEndpoint ($GraphEndpoint = $endptOut.graphEndpoint) `
-	               -StorageEndpointSuffix ($StorageEndpointSuffix="$($env:USERDNSDOMAIN)".ToLowerInvariant()) `
-	               -AzureKeyVaultDnsSuffix ($AzureKeyVaultDnsSuffix="vault.$($env:USERDNSDOMAIN)".ToLowerInvariant()) 
+				-ActiveDirectoryEndpoint ($ActiveDirectoryEndpoint = $($endptOut.authentication.loginEndpoint) + "5454420b-2e38-4b9e-8b56-1712d321cf33" + "/") `
+				-ActiveDirectoryServiceEndpointResourceId ($ActiveDirectoryServiceEndpointResourceId = $($endptOut.authentication.audiences[0])) `
+				-ResourceManagerEndpoint ($ResourceManagerEndpoint = $("https://api.$env:USERDNSDOMAIN".ToLowerInvariant())) `
+				-GalleryEndpoint ($GalleryEndpoint = $endptOut.galleryEndpoint) `
+				-GraphEndpoint ($GraphEndpoint = $endptOut.graphEndpoint) `
+			   -StorageEndpointSuffix ($StorageEndpointSuffix="$($env:USERDNSDOMAIN)".ToLowerInvariant()) `
+			   -AzureKeyVaultDnsSuffix ($AzureKeyVaultDnsSuffix="vault.$($env:USERDNSDOMAIN)".ToLowerInvariant())
 
 ## Authenticate a user to the environment (you will be prompted during authentication)
-    $privateEnv = Get-AzureRmEnvironment 'AzureStackCloud'
+    $privateEnv = Get-AzureRmEnvironment $envName
     $privateAzure = Add-AzureRmAccount -Environment $privateEnv -Verbose
     Select-AzureRmProfile -Profile $privateAzure
 
@@ -24,9 +24,8 @@ $RGName = "myRG$myNum"
 $myLocation = "local"
 
 $templateFile= "azuredeploy.json"
-# Fix the GEN-* values in the azuredeploy.parameters.json before proceeding to next steps
 $templateParameterFile= "azuredeploy.parameters.json"
-# For SSHKeys use below parameters.json
+# For SSHKeys use below parameters.json. Fix the GEN-* values before proceeding to next steps
 #$templateParameterFile= "azuredeploy-sshkeys.parameters.json"
 
 # Create Resource Group for Template Deployment
--- mesos-windows-jumpbox/deployMesos.ps1
+++ mesos-windows-jumpbox/deployMesos.ps1
@@ -2,16 +2,16 @@
 $endptOut = Invoke-RestMethod "$("https://api.$env:USERDNSDOMAIN".ToLowerInvariant())/metadata/endpoints?api-version=1.0"
 $envName = "AzureStackCloud"
 Add-AzureRmEnvironment -Name ($envName) `
-	                -ActiveDirectoryEndpoint ($ActiveDirectoryEndpoint = $($endptOut.authentication.loginEndpoint) + $($endptOut.authentication.audiences[0]).Split("/")[-1] + "/") `
-	                -ActiveDirectoryServiceEndpointResourceId ($ActiveDirectoryServiceEndpointResourceId = $($endptOut.authentication.audiences[0])) `
-	                -ResourceManagerEndpoint ($ResourceManagerEndpoint = $("https://api.$env:USERDNSDOMAIN".ToLowerInvariant())) `
-	                -GalleryEndpoint ($GalleryEndpoint = $endptOut.galleryEndpoint) `
-	                -GraphEndpoint ($GraphEndpoint = $endptOut.graphEndpoint) `
-	               -StorageEndpointSuffix ($StorageEndpointSuffix="$($env:USERDNSDOMAIN)".ToLowerInvariant()) `
-	               -AzureKeyVaultDnsSuffix ($AzureKeyVaultDnsSuffix="vault.$($env:USERDNSDOMAIN)".ToLowerInvariant()) 
+				-ActiveDirectoryEndpoint ($ActiveDirectoryEndpoint = $($endptOut.authentication.loginEndpoint) + "5454420b-2e38-4b9e-8b56-1712d321cf33" + "/") `
+				-ActiveDirectoryServiceEndpointResourceId ($ActiveDirectoryServiceEndpointResourceId = $($endptOut.authentication.audiences[0])) `
+				-ResourceManagerEndpoint ($ResourceManagerEndpoint = $("https://api.$env:USERDNSDOMAIN".ToLowerInvariant())) `
+				-GalleryEndpoint ($GalleryEndpoint = $endptOut.galleryEndpoint) `
+				-GraphEndpoint ($GraphEndpoint = $endptOut.graphEndpoint) `
+			   -StorageEndpointSuffix ($StorageEndpointSuffix="$($env:USERDNSDOMAIN)".ToLowerInvariant()) `
+			   -AzureKeyVaultDnsSuffix ($AzureKeyVaultDnsSuffix="vault.$($env:USERDNSDOMAIN)".ToLowerInvariant())
 
 ## Authenticate a user to the environment (you will be prompted during authentication)
-    $privateEnv = Get-AzureRmEnvironment 'AzureStackCloud'
+    $privateEnv = Get-AzureRmEnvironment $envName
     $privateAzure = Add-AzureRmAccount -Environment $privateEnv -Verbose
     Select-AzureRmProfile -Profile $privateAzure
 
@@ -25,7 +25,7 @@ $myLocation = "local"
 
 $templateFile= "azuredeploy.json"
 $templateParameterFile= "azuredeploy.parameters.json"
-# For SSHKeys use below parameters.json
+# For SSHKeys use below parameters.json.Fix the GEN-* values before proceeding to next steps
 #$templateParameterFile= "azuredeploy-sshkeys.parameters.json"
 
 # Create Resource Group for Template Deployment
