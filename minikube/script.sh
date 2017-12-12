1102768914b33859ae428da93ea540958f3ba74b
Vaibhav Patel <vapa@microsoft.com>
2017-12-12 15:33:12 -0800

    adding auto yes option for apt-get commands

--- minikube/script.sh
+++ minikube/script.sh
@@ -2,11 +2,11 @@ echo "Starting Minikube Deployment..."
 
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
 curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
-sudo apt-get update
+sudo apt-get -y update
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install docker-ce
+sudo apt-get -y update
+sudo apt-get -y install docker-ce
 sudo minikube start --vm-driver=none
 
 echo "Minikube Deployment Done"
