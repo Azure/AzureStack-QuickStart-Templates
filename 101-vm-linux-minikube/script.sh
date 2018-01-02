ed18ca34088af0e70c127bb93691d15d5bbd4618
vpatelsj <vpatelsj@gmail.com>
2018-01-02 14:36:24 -0800

    Update script.sh

--- 101-vm-linux-minikube/script.sh
+++ 101-vm-linux-minikube/script.sh
@@ -1,12 +1,41 @@
-echo "Starting Minikube Deployment..."
+set -e
 
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
-curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
+echo "Starting Minikube Deployment..."
+date
+
+echo "Running as:"
+whoami
+
+sleep 20
+
+echo "downloading minikube binary"
+sudo curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && sudo chmod +x minikube && sudo mv minikube /usr/local/bin/
+
+echo "downloading kubectl binary"
+sudo curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && sudo chmod +x kubectl &&  sudo mv kubectl /usr/local/bin/
+
+echo "update the system"
 sudo apt-get -y update
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+echo "add docker repo key"
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+echo "add docker repo" 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+echo "re-update the system"
 sudo apt-get -y update
+
+echo "install docker"
 sudo apt-get -y install docker-ce
+
+echo "start minikube"
 sudo minikube start --vm-driver=none
+sleep 20
+
+sudo kubectl cluster-info | grep 'running' &> /dev/null
+if [ $? == 0 ]; then
+   echo "Minikube started successfully"
+fi
 
 echo "Minikube Deployment Done"
