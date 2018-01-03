1f7b423257e8b69d61523c23080d0f96abc2ce61
vpatelsj <vpatelsj@gmail.com>
2018-01-02 23:29:09 -0800

    Update script.sh

--- 101-vm-linux-minikube/script.sh
+++ 101-vm-linux-minikube/script.sh
@@ -7,21 +7,28 @@ echo "Running as:"
 whoami
 
 sleep 20
+#############
+# Parameters
+#############
+MINIKUBELINK=${1}
+KUBECTLLINK=${2}
+DOCKERLINK=${3}
+
 
 echo "downloading minikube binary"
-sudo curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && sudo chmod +x minikube && sudo mv minikube /usr/local/bin/
+sudo curl -Lo minikube $MINIKUBELINK && sudo chmod +x minikube && sudo mv minikube /usr/local/bin/
 
 echo "downloading kubectl binary"
-sudo curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && sudo chmod +x kubectl &&  sudo mv kubectl /usr/local/bin/
+sudo curl -Lo kubectl $KUBECTLLINK && sudo chmod +x kubectl &&  sudo mv kubectl /usr/local/bin/
 
 echo "update the system"
 sudo apt-get -y update
 
 echo "add docker repo key"
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo curl -fsSL $DOCKERLINK/gpg | sudo apt-key add -
 
 echo "add docker repo" 
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo add-apt-repository "deb [arch=amd64] $DOCKERLINK $(lsb_release -cs) stable"
 
 echo "re-update the system"
 sudo apt-get -y update
@@ -37,5 +44,23 @@ sudo kubectl cluster-info | grep 'running' &> /dev/null
 if [ $? == 0 ]; then
    echo "Minikube started successfully"
 fi
+
+echo "Install xfce4"
+sudo apt-get -y update
+
+echo "Install xfce4"
+sudo apt-get -y install xfce4
+
+echo "Install xrdp"
+sudo apt-get -y install xrdp
+
+echo "Configure xsession"
+sudo echo xfce4-session >~/.xsession
+
+echo "Restart xrdp"
+sudo service xrdp restart
+
+echo "Install Firefox"
+sudo apt-get -y install firefox
 
 echo "Minikube Deployment Done"
