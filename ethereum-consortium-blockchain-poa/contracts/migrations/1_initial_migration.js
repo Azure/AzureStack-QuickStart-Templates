aba1b670b3b98d798bad1cfd772d1b91e27b9754
seyadava@microsoft.com <seyadava@microsoft.com>
2019-01-15 15:32:39 -0800

    remove unwanted

--- ethereum-consortium-blockchain-poa/contracts/migrations/1_initial_migration.js
+++
@@ -1,7 +0,0 @@
-var TestValidatorSet = artifacts.require(".\\contracts\\TestValidatorSet.sol");
-var AdminValidatorSet = artifacts.require(".\\contracts\\AdminValidatorSet.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(TestValidatorSet);
-  deployer.deploy(AdminValidatorSet);
-};
