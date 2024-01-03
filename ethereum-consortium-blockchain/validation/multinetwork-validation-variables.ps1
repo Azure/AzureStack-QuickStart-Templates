e693b58df8636b359ddf7ad9a150e2b8cb59f887
Krishna Nithin <knithinc@users.noreply.github.com>
2024-01-03 09:13:29 -0800

    Update multinetwork-validation-variables.ps1 (#492)
    Remove private data. Case ID: 84639

--- ethereum-consortium-blockchain/validation/multinetwork-validation-variables.ps1
+++ ethereum-consortium-blockchain/validation/multinetwork-validation-variables.ps1
@@ -4,9 +4,9 @@
 
 
 # Subscription under which resources will be deployed
-$SUBSCRIPTION_ID            = "922bb5fb-9ac3-4aa7-9a6b-f965aa49e6a3"; # "Azure Blockchain Service Runners" subscription
+$SUBSCRIPTION_ID            = ""; # "Azure Blockchain Service Runners" subscription
 # Azure Location where resource group and all resources will be deployed in (format e.g. "Central US" or "centralus")
-$LOCATION                 = "southcentralus";
+$LOCATION                 = "";
 # Root folder within which all template files that are being validated are contained. Determine latest path via test
 # deployment of published marketplace solution.
 $BASE_URL="https://gallery.azure.com/artifact/20161101/microsoft-azure-blockchain.azure-multi-member-blockchain-service-previewethereum-consortium-leader.1.0.1/Artifacts"
@@ -14,9 +14,9 @@ $LEADER_TEMPLATE_URI = $BASE_URL+"/mainTemplate.json";
 $JOINING_TEMPLATE_URI = "https://gallery.azure.com/artifact/20161101/microsoft-azure-blockchain.azure-multi-member-blockchain-service-previewethereum-consortium-member.1.0.5/Artifacts/mainTemplate.json";
 
 $NOTIFICATION_ENABLED     = "FALSE"; # Any value other than "TRUE" will disable notification
-$NOTIFICATION_USER_NAME   = "azure_bc8d0746cc32dcb3aceee23ddb70a0f7@azure.com"; # Username of sendgrid account
+$NOTIFICATION_USER_NAME   = ""; # Username of sendgrid account
 $NOTIFICATION_PASSWORD    = ""; # Password of sendgrid account
-$NOTIFICATION_ALIAS       = "azblockchain-icm-trg@microsoft.com";
+$NOTIFICATION_ALIAS       = "";
 
 $authType      = "password";
 $vmAdminPasswd = "";
